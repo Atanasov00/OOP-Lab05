690cbdeff947a691b0f1f24eb352dd94dda4633c
Atanas Todorov Atanasov <atanastodor.atanasov@studio.unibo.it>
2021-11-11 15:05:41 +0100

    Develop the class UseSet

--- src/it/unibo/oop/lab05/ex1/UseSet.java
+++ src/it/unibo/oop/lab05/ex1/UseSet.java
@@ -1,6 +1,9 @@
 package it.unibo.oop.lab05.ex1;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * Example class using {@link Set}.
@@ -34,5 +37,40 @@ public final class UseSet {
          * 
          * 6) Verifies if all the numbers left in the set are even
          */
+    	final int DIVISIBLE_NUMBER = 3;
+    	final Collection<String> collString = new TreeSet<>();
+    	collString.addAll(Arrays.asList("1", "2", "3", "4", "5","6", "7", "8", "9", 
+    			"10","11", "12", "13", "14", "15","16", "17", "18", "19", "20"));
+    	System.out.println(collString);
+    	/*for(String s: collString) {
+    		if((Integer.parseInt(s) % DIVISIBLE_NUMBER) == 0) {
+    			collString.remove(s);
+    		}
+    	}*/
+    	var iterator = collString.iterator();
+    	while (iterator.hasNext()) {
+    		if((Integer.parseInt(iterator.next()) % DIVISIBLE_NUMBER) == 0){
+    			iterator.remove();
+    		}
+    	}
+    	
+    	for(String s: collString) {
+    			System.out.print(s + "|");
+    	}
+    	
+    	int cont = 0;
+	    for(String s: collString) {
+			if((Integer.parseInt(s) % 2) != 0) {
+				System.out.println("\nNon tutti i numeri all'interno del set sono pari.");
+				break;
+			}
+			else {
+				cont++;
+			}
+		}
+	    
+	    if(cont == collString.size()) {
+	    	System.out.println("Tutti i numeri all'interno del set sono pari.");
+	    }
     }
 }
