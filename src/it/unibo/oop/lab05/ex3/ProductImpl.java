c5bf40c70d049aec0e9b14da92169cfc667c1cbd
Atanas Todorov Atanasov <atanastodor.atanasov@studio.unibo.it>
2021-11-11 16:03:19 +0100

    add ProductImpl and develop

---
+++ src/it/unibo/oop/lab05/ex3/ProductImpl.java
@@ -0,0 +1,23 @@
+package it.unibo.oop.lab05.ex3;
+
+public class ProductImpl implements Product {
+	
+	private final String name;
+	private final double quantity;
+	
+	public ProductImpl(final String name, final double quantity) {
+		this.name = name;
+		this.quantity = quantity;
+	}
+	
+	@Override
+	public String getName() {
+		return this.name;
+	}
+
+	@Override
+	public double getQuantity() {
+		return this.quantity;
+	}
+
+}
