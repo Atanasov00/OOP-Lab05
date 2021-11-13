a9d284f3c95185e88dbaed6a97b4d243f28f7e6e
Atanas Todorov Atanasov <atanastodor.atanasov@studio.unibo.it>
2021-11-13 10:57:37 +0100

    delete useless import

--- src/it/unibo/oop/lab05/ex5/Utilities.java
+++ src/it/unibo/oop/lab05/ex5/Utilities.java
@@ -2,7 +2,6 @@ package it.unibo.oop.lab05.ex5;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
